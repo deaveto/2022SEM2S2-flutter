88aff2296253c83caccbfd9caee11e13feb2265f
deaveto <53503206+deaveto@users.noreply.github.com>
2022-09-10 11:05:21 -0500

    Create FilaBotones.dart

---
+++ lib/widget/FilaBotones.dart
@@ -0,0 +1,15 @@
+import 'package:calculadora/models/boton.models.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+Widget filaBoton(List<modeloBoton> lista) {
+  List<Widget> listabotones = [];
+  lista.forEach((element) {
+    listabotones.add(ElevatedButton(
+        onPressed: element.metodo, child: Text(element.titulos)));
+  });
+
+  return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: listabotones); // espacio entre filass
+}
